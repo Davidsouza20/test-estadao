e50b98167759a13ab5d932c085c4ff2083cec935
david.souza <david.souza@o2obots.com>
2024-11-11 19:13:21 -0300

    try call auth endpoint

--- web/src/http/auth.ts
+++ web/src/http/auth.ts
@@ -1,3 +1,4 @@
+// import { UserLogin } from "@/types/user.types";
 // import axios from "axios";
 
 // const apiClient = axios.create({
@@ -16,11 +17,12 @@ export const login = async (
   //   password,
   // });
 
+
   Promise.resolve(setTimeout(() => {}, 1000));
   if (username === "john.doe@teste.com" && password === "siTe$_Deve!0pers@10") {
     return {
       token:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ3d3cuZXN0YWRhby5jb20iLCJhdWQiOiJUZXN0ZSBFc3RhZMOjbyBEZXNlbnZvbHZlZG9yZXMiLCJzdWIiOiJqb2huLmRvZUB0ZXN0ZS5jb20iLCJFbWFpbCI6ImpvaG4uZG9lQHRlc3RlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG4iLCJTdXJuYW1lIjoiRG9lIiwidXNlcm5hbWUiOiJqb2huLmRvZUB0ZXN0ZS5jb20iLCJpYXQiOjE3MzEzNDA0MDMsImV4cCI6MTczMTM0NDAwM30.3NNGLypkhZAb2bVFaUgsu-WFMOkvuxXYiqnAtTooFFs",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.",
     };
   }
 
